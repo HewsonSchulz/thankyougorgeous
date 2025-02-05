9cd9da4aa134beabf012b825acbd1a2012e7af27
HewsonSchulz <HewsonSchulz@users.noreply.github.com>
2025-02-05 17:54:12 +0000

    deploy: e45d592ddecfe86a5b1d5cfeb45be8b84e3a15b8

--- server/thankyougorgeousapi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThankyougorgeousapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'thankyougorgeousapi'
--- server/thankyougorgeousapi/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .user import User
-from .email_code import EmailCode
-from .category import Category
-
-# from .interest import Interest
-from .product import Product
-from .order import Order
-from .order_product import OrderProduct
