3e1bcee82ea35c0224d4390a5e1c6faa3bd00d4a
Hewson Schulz <hewsonschulz@gmail.com>
2025-02-07 18:44:34 -0600

    Update AWS_STORAGE_BUCKET_NAME

--- server/thankyougorgeousproject/cdn/conf.py
+++ server/thankyougorgeousproject/cdn/conf.py
@@ -3,7 +3,7 @@ import os
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = 'hewson-space'
+AWS_STORAGE_BUCKET_NAME = 'thankyougorgeous-bucket'
 AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
 AWS_LOCATION = 'https://thankyougorgeous-bucket.nyc3.digitaloceanspaces.com'
 
