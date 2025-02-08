dde082cc40347f4fc7047dc32a433fccd3aeaa4c
Hewson Schulz <hewsonschulz@gmail.com>
2025-02-07 18:40:08 -0600

    Update AWS_LOCATION

--- server/thankyougorgeousproject/cdn/conf.py
+++ server/thankyougorgeousproject/cdn/conf.py
@@ -5,7 +5,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 AWS_STORAGE_BUCKET_NAME = 'hewson-space'
 AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-AWS_LOCATION = 'https://hewson-space.nyc3.digitaloceanspaces.com'
+AWS_LOCATION = 'https://thankyougorgeous-bucket.nyc3.digitaloceanspaces.com'
 
 STATICFILES_STORAGE = 'thankyougorgeousproject.cdn.backends.StaticRootS3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'thankyougorgeousproject.cdn.backends.MediaRootS3Boto3Storage'
